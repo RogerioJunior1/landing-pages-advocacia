d433e0842f6e8aa44e2553405785b809b98af596
RogerioJunior1 <rogeriocbj@adv.oabsp.org.br>
2026-04-16 20:40:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
 
-// Import landing page components
+// Importando todas as páginas do seu site
+import Home from './components/Home'
 import DireitoConsumidor from './components/DireitoConsumidor'
 import DireitoCivil from './components/DireitoCivil'
-import IndenizacoesCobracas from './components/IndenizacoesCobracas'
 import DireitoImobiliario from './components/DireitoImobiliario'
 import DireitoEmpresarial from './components/DireitoEmpresarial'
-import Home from './components/Home'
+import IndenizacoesCobrancas from './components/IndenizacoesCobrancas'
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/direito-consumidor" element={<DireitoConsumidor />} />
-          <Route path="/direito-civil" element={<DireitoCivil />} />
-          <Route path="/indenizacoes-cobrancas" element={<IndenizacoesCobracas />} />
-          <Route path="/direito-imobiliario" element={<DireitoImobiliario />} />
-          <Route path="/direito-empresarial" element={<DireitoEmpresarial />} />
-        </Routes>
-      </div>
+      <Routes>
+        {/* Rota Principal */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Rotas das Áreas de Atuação */}
+        <Route path="/direito-consumidor" element={<DireitoConsumidor />} />
+        <Route path="/direito-civil" element={<DireitoCivil />} />
+        <Route path="/direito-imobiliario" element={<DireitoImobiliario />} />
+        <Route path="/direito-empresarial" element={<DireitoEmpresarial />} />
+        <Route path="/indenizacoes-cobrancas" element={<IndenizacoesCobrancas />} />
+      </Routes>
     </Router>
   )
 }
