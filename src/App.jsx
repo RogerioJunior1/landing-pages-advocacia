a316df3bc8c8e423bf5b9f109d6d062430b7cdf7
RogerioJunior1 <rogeriocbj@adv.oabsp.org.br>
2026-04-16 20:51:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './components/Home.jsx'
 import DireitoConsumidor from './components/DireitoConsumidor.jsx'
 import DireitoCivil from './components/DireitoCivil.jsx'
 import DireitoImobiliario from './components/DireitoImobiliario.jsx'
 import DireitoEmpresarial from './components/DireitoEmpresarial.jsx'
-// Desligamos a página de Indenizações temporariamente para não travar o Vercel
 
 function App() {
   return (
