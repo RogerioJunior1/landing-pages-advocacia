1effdedc834ab74445787e9174f4691e38c7a78e
RogerioJunior1 <rogeriocbj@adv.oabsp.org.br>
2026-04-16 21:07:13 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Home from './components/Home.jsx'
-import DireitoConsumidor from './components/DireitoConsumidor.jsx'
-import DireitoCivil from './components/DireitoCivil.jsx'
-import DireitoImobiliario from './components/DireitoImobiliario.jsx'
-import DireitoEmpresarial from './components/DireitoEmpresarial.jsx'
+import Home from './components/Home'
+import DireitoConsumidor from './components/DireitoConsumidor'
+import DireitoCivil from './components/DireitoCivil'
+import DireitoImobiliario from './components/DireitoImobiliario'
+import DireitoEmpresarial from './components/DireitoEmpresarial'
+import IndenizacoesCobrancas from './components/IndenizacoesCobrancas'
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Rota Principal */}
         <Route path="/" element={<Home />} />
-        
-        {/* Rotas das Áreas de Atuação */}
         <Route path="/direito-consumidor" element={<DireitoConsumidor />} />
         <Route path="/direito-civil" element={<DireitoCivil />} />
         <Route path="/direito-imobiliario" element={<DireitoImobiliario />} />
         <Route path="/direito-empresarial" element={<DireitoEmpresarial />} />
+        <Route path="/indenizacoes-cobrancas" element={<IndenizacoesCobrancas />} />
       </Routes>
     </Router>
   )
