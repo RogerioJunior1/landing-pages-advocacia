e40a4d5b1639ab45315c51aee1fdefaafea84a98
RogerioJunior1 <rogeriocbj@adv.oabsp.org.br>
2026-04-16 23:18:21 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // ESTA LINHA É A MAIS IMPORTANTE!
+import './index.css' // ESTA LINHA TRAZ O DESIGN DE VOLTA
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
