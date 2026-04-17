ebd207152d5adb1b48035be64a79ee34d7cde822
RogerioJunior1 <rogeriocbj@adv.oabsp.org.br>
2026-04-17 20:04:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import DireitoConsumidor from './components/DireitoConsumidor'
 import DireitoCivil from './components/DireitoCivil'
 import DireitoImobiliario from './components/DireitoImobiliario'
 import DireitoEmpresarial from './components/DireitoEmpresarial'
-// Omitimos a página de indenizações temporariamente para não dar erro
+import IndenizacoesCobracas from './components/IndenizacoesCobracas'
 
 function App() {
   return (
@@ -16,6 +16,10 @@ function App() {
         <Route path="/direito-civil" element={<DireitoCivil />} />
         <Route path="/direito-imobiliario" element={<DireitoImobiliario />} />
         <Route path="/direito-empresarial" element={<DireitoEmpresarial />} />
+        <Route
+          path="/indenizacoes-cobrancas"
+          element={<IndenizacoesCobracas />}
+        />
       </Routes>
     </Router>
   )
