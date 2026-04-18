880940100c9e5c609477311e58b22c430a1caa0d
RogerioJunior1 <rogeriocbj@adv.oabsp.org.br>
2026-04-18 17:10:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import DireitoConsumidor from './components/DireitoConsumidor'
 import DireitoCivil from './components/DireitoCivil'
 import DireitoImobiliario from './components/DireitoImobiliario'
 import DireitoEmpresarial from './components/DireitoEmpresarial'
-// Omitimos a página de indenizações temporariamente para não dar erro
 import IndenizacoesCobracas from './components/IndenizacoesCobracas'
 
 function App() {
   return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/direito-consumidor" element={<DireitoConsumidor />} />
         <Route path="/direito-civil" element={<DireitoCivil />} />
         <Route path="/direito-imobiliario" element={<DireitoImobiliario />} />
         <Route path="/direito-empresarial" element={<DireitoEmpresarial />} />
@@ -16,3 +23,6 @@ function App() {
       </Routes>
     </Router>
   )
+}
+
+export default App
